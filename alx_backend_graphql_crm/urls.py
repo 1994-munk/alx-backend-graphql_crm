3dcb61dc470e41f6d856ad3afcc46e54c31f8fcd
Shirleen Gatu <wambuimunk@gmail.com>
2025-10-17 07:08:55 +0300

    move schema.py

--- alx_backend_graphql_crm/urls.py
+++ alx_backend_graphql_crm/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt  # Disable CSRF for GraphQL testing
+from alx_backend_graphql_crm.schema import schema  # ✅ Import your schema
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
